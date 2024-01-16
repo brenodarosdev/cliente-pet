04604579ce9ca6ca8e977cd14f3d0d14b99dee70
Breno Darós <brenosouzadaros@gmail.com>
2024-01-16 11:51:02 -0300

    implementa api get cliente através do ID

---
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteDetalhadoResponse.java
@@ -0,0 +1,18 @@
+package br.com.petz.clientepet.cliente.application.api;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import br.com.petz.clientepet.cliente.domain.Sexo;
+
+public class ClienteDetalhadoResponse {
+	private UUID idCliente;
+	private String nomeCompleto;
+	private String cpf;
+	private String email;
+	private String celular;
+	private String telefone;
+	private Sexo sexo;
+	private Boolean aceitaTermos;
+	private LocalDateTime dataHoraDoCadastro;
+}
